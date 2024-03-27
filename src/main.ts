03932b58f1d5f039148fad41162bd212f1e373a5
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-03-27 17:47:14 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001', // Substitua pela origem do seu front-end, se diferente
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
